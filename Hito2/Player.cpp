7f56440001c0c2e35b3098f4154f267abf9cdcc5
GianfrancoMS <gianfrancomonsal@gmail.com>
2017-07-04 16:07:55 -0500

    Update player

--- Hito2/Player.cpp
+++ Hito2/Player.cpp
@@ -1,80 +1,60 @@
 #include "Player.h"
-#include "Board.h"
+#include "Utils.h"
 
-Player::Player(PlayerColor color, Board board)
+Player::Player(PlayerColor color)
 {
 	this->color = color;
-	int begin = 0;
-	int end = 0;
-	if (color == PlayerColor::PLAYER_WHITE) {
-		begin = 0;
-		end = 3;
-	}
-	else {
-		begin = 5;
-		end = 8;
-	}
-	for (auto x = begin; x < end; ++x) {
-		for (auto y = x % 2 == 0 ? 0 : 1; y < board.COLUMNS; y += 2) {
-			Draught draught;
-			draught.point.x = x;
-			draught.point.y = y;
-			draught.draughtType = DraughtType::NORMAL;
-			draught.playerColor = this->color;
-			draughts.insert(draught);
-		}
-	}
 }
 
-void Player::removeDraught(Draught draught)
-{
-	for each (Draught obj in draughts)
-	{
-		if (obj.point.x == draught.point.x && obj.point.y == draught.point.y)
-		{
-			draught = obj;
-		}
-	}
-	draughts.erase(draught);
-}
-
-void Player::upgradeToQueen(Draught  draught)
-{
-	auto queen = *draughts.find(draught);
-	queen.draughtType = DraughtType::QUEEN;
-	removeDraught(draught);
-	draughts.insert(queen);
-}
-
-void Player::updatePosition(Draught draught, int x, int y)
-{
-	auto newDraught = *draughts.find(draught);
-	newDraught.point.x = x;
-	newDraught.point.y = y;
-	removeDraught(draught);
-	if (newDraught.playerColor == PlayerColor::PLAYER_RED && newDraught.point.x == 0)
-	{
-		upgradeToQueen(newDraught);
-	}
-	else if (newDraught.playerColor == PlayerColor::PLAYER_WHITE && newDraught.point.x == 7)
-	{
-		upgradeToQueen(newDraught);
-	}
-	else
-	{
-		draughts.insert(newDraught);
-	}
-}
-
-Draught Player::getDraught(int x, int y)
-{
-	Draught draught;
-	for each (Draught obj in draughts)
-	{
-		if (obj.point.x == x && obj.point.y == y)
-		{
-			draught = obj;
-		}
-	}
-	return draught;
-}
+//void Player::removeDraught(Draught draught)
+//{
+//	for each (Draught obj in draughts)
+//	{
+//		if (obj.point.x == draught.point.x && obj.point.y == draught.point.y)
+//		{
+//			draught = obj;
+//		}
+//	}
+//	draughts.erase(draught);
+//}
+//
+//void Player::upgradeToQueen(Draught  draught)
+//{
+//	auto queen = *draughts.find(draught);
+//	queen.draughtType = DraughtType::QUEEN;
+//	removeDraught(draught);
+//	draughts.insert(queen);
+//}
+//
+//void Player::updatePosition(Draught draught, int x, int y)
+//{
+//	auto newDraught = *draughts.find(draught);
+//	newDraught.point.x = x;
+//	newDraught.point.y = y;
+//	removeDraught(draught);
+//	if (newDraught.playerColor == PlayerColor::PLAYER_RED && newDraught.point.x == 0)
+//	{
+//		upgradeToQueen(newDraught);
+//	}
+//	else if (newDraught.playerColor == PlayerColor::PLAYER_WHITE && newDraught.point.x == 7)
+//	{
+//		upgradeToQueen(newDraught);
+//	}
+//	else
+//	{
+//		draughts.insert(newDraught);
+//	}
+//}
+//
+//Draught Player::getDraught(int x, int y)
+//{
+//	Draught draught;
+//	for each (Draught obj in draughts)
+//	{
+//		if (obj.point.x == x && obj.point.y == y)
+//		{
+//			draught = obj;
+//		}
+//	}
+//	return draught;
+//}
--- Hito2/Player.h
+++ Hito2/Player.h
@@ -1,25 +1,12 @@
 #pragma once
-#include <set>
-#include "Utils.h"
 
 using namespace std;
-
-struct Draught;
-class Board;
 
 class Player
 {
 public:
-	Player(PlayerColor color, Board board);
+	Player(PlayerColor color);
 	~Player()=default;
-
 	PlayerColor color;
-
-	void removeDraught(Draught C);
-	void upgradeToQueen(Draught draught);
-	void updatePosition(Draught draught, int x, int y);
-	Draught getDraught(int x, int y);
-	
-	set<Draught> draughts;
 };
 

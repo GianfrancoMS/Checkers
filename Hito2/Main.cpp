035214305dccadbd0fdf541737de2ca5e6c0c9d9
GianfrancoMS <gianfrancomonsal@gmail.com>
2017-07-04 20:20:35 -0500

    Fixed Board::move

--- Hito2/Main.cpp
+++ Hito2/Main.cpp
@@ -4,44 +4,58 @@
 
 int main() {
 	Board board = Board();
+
 	Player playerWHITE = Player(PlayerColor::PLAYER_WHITE);
 	Player playerRED = Player(PlayerColor::PLAYER_RED);
 
-	/*board.printBoard();*/
-	//cout << "Posiciones de las Fichas Blancas:  "<<endl;
-	//for (auto draught : playerWHITE.draughts)
-	//	cout << draught.point.x << " " << draught.point.y << endl;
-	//cout << "Posiciones de las FIchas Rojas:  " << endl;
-	//for (auto draught : playerRED.draughts)
-	//	cout << draught.point.x << " " << draught.point.y << endl;
+	Draught draught;
+	int x = 0; 
+	int y = 0;
+	int aux = 0;
+	Move move;
 
-	Draught obj;
-	int x, y;
-	int aux;
-	Move mover;
-	obj = board.getDraught(2, 6, playerWHITE.color);
-	board.move(playerWHITE, obj, Move::DIAGONAL_DOWN_LEFT);
-	obj = board.getDraught(3, 5, playerWHITE.color);
-	board.move(playerWHITE, obj, Move::DIAGONAL_DOWN_RIGHT);
+	draught = board.getDraught(playerWHITE.color, 2 ,6);
+	board.move(playerWHITE, draught, Move::DIAGONAL_DOWN_LEFT);
+
+	draught = board.getDraught(playerWHITE.color, 3, 5);
+	board.move(playerWHITE, draught, Move::DIAGONAL_DOWN_RIGHT);
+
 	board.printBoard();
+
 	while (true)
 	{
-		cout << "x:"; cin >> x; cout << "Y:"; cin >> y; cout << "Ezquierda [1], Derecha[2] :"; cin >> aux; cout << endl;
+		auto possiblePlaysWhite = board.possibleMoves(playerWHITE);
+		auto possiblePlaysRed = board.possibleMoves(playerRED);
+
+		cout << "White moves: " << endl;
+		for (auto draught : possiblePlaysWhite)
+			cout << draught.point.x << " " << draught.point.y << endl;
+
+		cout << "Red moves: " << endl;
+		for (auto draught : possiblePlaysRed)
+			cout << draught.point.x << " " << draught.point.y << endl;
+
+		cout << "X:"; 
+		cin >> x; 
+		cout << "Y:"; 
+		cin >> y; 
+		cout << "Izquierda [1], Derecha[2] :"; 
+		cin >> aux; 
+		cout << endl;
 		if (aux == 1)
-			mover = Move::DIAGONAL_TOP_LEFT;
+			move = Move::DIAGONAL_TOP_LEFT;
 		else
-			mover = Move::DIAGONAL_TOP_RIGHT;
-		obj = board.getDraught(x, y, playerRED.color);
-		if (board.move(playerRED, obj, mover) != MoveStatus::MOVE_IMPOSSIBLE)
+			move = Move::DIAGONAL_TOP_RIGHT;
+		draught = board.getDraught(playerRED.color,x,y);
+		if (board.move(playerRED, draught, move) != MoveStatus::MOVE_IMPOSSIBLE)
 		{
 			board.printBoard();
+			cout << "White draughts: " << endl;
 			for (auto draught : board.whiteDraughts)
 				cout << draught.point.x << " " << draught.point.y << endl;
 		}
 		else
-		{
 			cout << "jugada incorrecta" << endl;
-		}
 	}
 
 	cin.get();
